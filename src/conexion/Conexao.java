916e4e4ec51e8f113f88fa29854bf6aaf661f915
laysonbatista <laysonbatista3@gmail.com>
2025-10-16 03:38:21 -0300

    Modifying Conexao.java and Adding mysql-connector

--- src/conexion/Conexao.java
+++ src/conexion/Conexao.java
@@ -1,3 +1,5 @@
+package conexion;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,7 +9,7 @@ public class Conexao {
     // Dados de conexão com o banco MySQL
     private static final String URL = "jdbc:mysql://localhost:3306/labdatabase";
     private static final String USER = "labdatabase";
-    private static final String PASSWORD = "labdatabase"; 
+    private static final String PASSWORD = "lab@Database2025"; 
 
     /**
      * Cria e retorna uma conexão com o banco de dados.
